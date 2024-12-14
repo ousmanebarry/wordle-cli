f5ceb32b848430cb42650a7b4df8bc0cb7e2515f
Ousmane Barry <ppobarry@gmail.com>
2024-12-14 17:05:35 -0500

    v0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pywordle-cli",
-    version="0.1.2",
+    version="0.1.3",
     description="A command line based wordle game",
     long_description=long_description,
     long_description_content_type="text/markdown",
